c22398308492f0abdceb2739f6f56f97c431d1c7
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2023-10-25 05:07:05 +0000

    [Project done]

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const suits = ["club", "spade", "diamond", "heart"];
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+const card = document.getElementById("card");
+const btnGenerate = document.getElementById("btnGenerate");
+
+window.onload = generateCard;
+
+// =================== LISTENERS ===================== //
+btnGenerate.addEventListener("click", generateCard);
+
+// =================== FUNCTIONS ===================== //
+function generateCard() {
+  const randomSuit = getRandomElement(suits);
+  const redSuit = randomSuit === "heart" || randomSuit === "diamond" || false;
+
+  const randomNumber = getRandomElement(numbers);
+
+  const htmlContent = `
+    <span class="${
+      redSuit ? "card_symbol redColor" : "card_symbol"
+    }"><i class="bi bi-suit-${randomSuit}-fill"></i></span>
+    <span class="card_number my-2">${randomNumber}</span>
+    <span class="${
+      redSuit ? "card_symbol redColor" : "card_symbol"
+    }"><i class="bi bi-suit-${randomSuit}-fill"></i></span>
+  `;
+
+  card.innerHTML = htmlContent;
+}
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
